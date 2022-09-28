5e5302a927fdaee32cb7b1b700a4a5fe6615727e
Ranger <1463940971@qq.com>
2022-09-28 17:44:34 +0800

    09/28

--- OpenGLTemplate/OpenGLTemplate/main.cpp
+++ OpenGLTemplate/OpenGLTemplate/main.cpp
@@ -5,55 +5,157 @@
 #include <iostream>
 #include <fstream>
 #include "Utils.h"
+#include <math.h>
 using namespace std;
 
-#define numVAOs 1
-#define numVBOs 1
+#define numVAOs 4
+#define numVBOs 5
+
+#define M_PI acos(-1)
 
 //全局变量
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
+
+const int ELLIPSE_NUM_POINTS = 100;
+const int LINE_NUM_POINTS = 2;
+
 GLuint vColorLoc, vPositionLoc;
+
+
+// 根据角度生成颜色
+float generateAngleColor(double angle)
+{
+	return 1.0 / (2 * M_PI) * angle;
+}
+
+// 计算椭圆/圆上的点
+glm::vec2 getEllipseVertex(glm::vec2 center, double scale, double verticalScale, double angle)
+{
+	glm::vec2 vertex(sin(angle), cos(angle));
+	vertex *= scale;
+	vertex.y *= verticalScale;
+	vertex += center;
+	return vertex;
+}
+
+// 获得椭圆/圆的每个顶点
+void generateEllipsePoints(glm::vec2 vertices[], glm::vec3 colors[], int startVertexIndex, int numPoints,
+	glm::vec2 center, double scale, double verticalScale)
+{
+	double angleIncrement = (2 * M_PI) / numPoints;
+	double currentAngle = M_PI / 2;
+
+	for (int i = startVertexIndex; i < startVertexIndex + numPoints; ++i) {
+		vertices[i] = getEllipseVertex(center, scale, verticalScale, currentAngle);
+		if (verticalScale == 1.0) { //圆形
+			colors[i] = glm::vec3(generateAngleColor(currentAngle), 0.0, 0.0);
+		}
+		else { //椭圆
+			colors[i] = glm::vec3(generateAngleColor(currentAngle), 0.0, 0.0);//RED;
+		}
+		currentAngle += angleIncrement;
+	}
+}
 
 //导入着色器，初始化窗口
 void init(GLFWwindow* window)
 {
-	//编译、链接着色器程序
+
+	// 定义线的点
+	glm::vec2 line_vertices[LINE_NUM_POINTS];
+	glm::vec3 line_colors[LINE_NUM_POINTS];
+
+	//生成椭圆
+	glm::vec2 ellipse_vertices[ELLIPSE_NUM_POINTS];
+	glm::vec3 ellipse_colors[ELLIPSE_NUM_POINTS];
+	glm::vec2 ellipsecenter(0, 0);
+	generateEllipsePoints(ellipse_vertices, ellipse_colors, 0, ELLIPSE_NUM_POINTS, ellipsecenter, 0.6, 0.9);
+
 	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
 
-	float vertices[9] = {
-		-0.5f, -0.5f,0.0f,
-		0.5f,-0.5f, 0.0f,
-		0.0f, 0.5f,0.0f
-	};
+	// 初始化椭圆
+	glGenVertexArrays(1, &vao[1]);
+	glBindVertexArray(vao[1]);
 
-	glGenVertexArrays(numVAOs, vao);
-	glGenBuffers(numVBOs, vbo);
-
-	// Associate out shader variables with our data buffer    
-	vPositionLoc = glGetAttribLocation(renderingProgram, "vPosition");
-
-	//绑定当前VAO
-	glBindVertexArray(vao[0]);
-	// Load the data into the GPU  
+	glGenBuffers(1, &vbo[0]);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//将VBO关联给顶点着色器中相应的顶点属性
-	glVertexAttribPointer(vPositionLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(vPositionLoc);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(ellipse_vertices), ellipse_vertices, GL_STATIC_DRAW);
+	GLuint location = glGetAttribLocation(renderingProgram, "vPosition");
+	glEnableVertexAttribArray(location);
+	glVertexAttribPointer(
+		location,
+		2,
+		GL_FLOAT,
+		GL_FALSE,
+		sizeof(glm::vec2),
+		0);
 
+	glGenBuffers(1, &vbo[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(ellipse_colors), ellipse_colors, GL_STATIC_DRAW);
+	GLuint cLocation = glGetAttribLocation(renderingProgram, "vColor");
+	glEnableVertexAttribArray(cLocation);
+	glVertexAttribPointer(
+		cLocation,
+		3,
+		GL_FLOAT,
+		GL_FALSE,
+		sizeof(glm::vec3),
+		0);
+
+
+	float vertices[9] = {
+		-0.25f, 0.4f, 0.0f,
+		0.5f, 0.6f, 0.0f,
+		0.6f, 0.0f, 0.0f
+	};
+	glGenVertexArrays(1, &vao[2]);
+	glGenBuffers(1, &vbo[3]);
+
+	glBindVertexArray(vao[2]);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(vPositionLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(location);
+
+	float vertices2[9] = {
+		0.25f, 0.4f, 0.0f,
+		-0.5f, 0.6f, 0.0f,
+		-0.6f, 0.0f, 0.0f
+	};
+	glGenVertexArrays(1, &vao[3]);
+	glGenBuffers(1, &vbo[4]);
+
+	glBindVertexArray(vao[3]);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[4]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glVertexAttribPointer(vPositionLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(location);
 }
 //函数绘制
 void display(GLFWwindow* window, double currentTime)
 {
 	glUseProgram(renderingProgram);
 
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	//绘制模型
-	glBindVertexArray(vao[0]);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	//绘制椭圆
+	glBindVertexArray(vao[1]);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, ELLIPSE_NUM_POINTS);
+
+
+	glBindVertexArray(vao[2]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+	glBindVertexArray(vao[3]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main(void)
@@ -64,30 +166,27 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 	//窗口版本
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); //主版本号
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //次版本号
-
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	//创建窗口
-	GLFWwindow* window = glfwCreateWindow(800, 800, "simple", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "A triangle", NULL, NULL);
 	glfwMakeContextCurrent(window);
-	//设置缓存刷新时间
-	glfwSwapInterval(1);
 
 	if (glewInit() != GLEW_OK)//glew初始化
 	{
 		exit(EXIT_FAILURE);
 	}
+	glfwSwapInterval(1);
 
 	//导入着色器，创建和绑定VAO和VBO
 	init(window);
 
-	//事件循环，接收输入事件
+	//事件循环
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
-		display(window, glfwGetTime()); //绘制函数，主体
-		glfwSwapBuffers(window); //交换颜色缓存
-		glfwPollEvents(); // 检查有没有触发什么事件（比如键盘输入、鼠标移动等）
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(window);
